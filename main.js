f20fd52cb1670148f5c31226f3d17b2ddfc0cdbc
vai na web <vainawebcpx@gmail.com>
2019-10-30 15:01:26 -0300

    Incompleto

---
+++ main.js
@@ -0,0 +1,13 @@
+var entrada = document.getElementById('input') 
+console.log(entrada)
+var saida = document.getElementById('h1')
+console.log(saida)
+
+function enviar(event) {
+        event.preventDefault() 
+        console.log(event)
+        saida.innerHTML = entrada.value 
+}
+function cor(saida)  {
+        if value[> 0] = color[red]
+}
